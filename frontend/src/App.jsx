11e55be11c26c9ef92497ecbef775b9250c1044b
jarvis0198 <pranav.kumarrock143@gmail.com>
2025-06-26 18:10:50 +0530

    url changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io('http://localhost:8000', {
+      const socketio = io('https://pranav-social-media.onrender.com/', {
         query: {
           userId: user?._id
         },
